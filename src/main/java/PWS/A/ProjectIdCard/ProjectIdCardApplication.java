b350102f571e71a53db709fef0873285a383a44b
IsnaHifdzi <hifdziiwh@gmail.com>
2022-11-13 11:00:54 +0700

    First Project

---
+++ src/main/java/PWS/A/ProjectIdCard/ProjectIdCardApplication.java
@@ -0,0 +1,13 @@
+package PWS.A.ProjectIdCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIdCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIdCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PWS/A/ProjectIdCard/ProjectIdCardApplicationTests.java
@@ -0,0 +1,13 @@
+package PWS.A.ProjectIdCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIdCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
